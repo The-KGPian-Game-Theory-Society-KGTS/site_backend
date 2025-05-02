f1a8c08d82a4ea2c7bdc66788ef56b3c1653b777
Akshat-jwr <akshat2306jwr3@gmail.com>
2025-05-02 19:36:35 +0530

    User Model created

---
+++ src/models/event.models.js
@@ -0,0 +1,37 @@
+import mongoose, { VirtualType } from "mongoose";
+
+const eventSchema = new mongoose.Schema({
+    username : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim:true,
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim:true,
+    },
+    password: {
+        type: String,
+        required: [true,'Password is Required'],
+    },
+    registeredEvents: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Event"
+        }
+    ]
+
+    //Subject to Additions
+
+}, {timestamps:true});
+
+export const Event = mongoose.model("Event",eventSchema);
---
+++ src/models/user.models.js
@@ -0,0 +1,35 @@
+import mongoose, { VirtualType } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim:true,
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim:true,
+    },
+    password: {
+        type: String,
+        required: [true,'Password is Required'],
+    },
+    registeredEvents: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Event"
+        }
+    ]
+
+}, {timestamps:true});
+
+export const User = mongoose.model("User",userSchema);
